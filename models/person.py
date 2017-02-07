cfc6698f5fbfc36a8c58026d172b5e1f36a00149
andela-oadeniran <oladipupo.adeniran@andela.com>
2017-02-08 00:55:56 +0100

    ft models and commands for task0

---
+++ models/person.py
@@ -0,0 +1,5 @@
+class Person(object):
+	"""docstring for Person: The class helps to create instances of the
+	person object at Andela's Dojo"""
+	def __init__(self, name):
+		self.name = name
---
+++ models/room.py
@@ -0,0 +1,6 @@
+class Room(object):
+	"""docstring for Room: The class creates room objects for
+	fellows and staff of Andela at The Dojo"""
+	def __init__(self, name, room_type):
+		self.name = name
+		self.room_type = room_type
